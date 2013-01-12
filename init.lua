b7efe72cefa5d35b81a8316ea586aac0026c1e84
jojoa1997 <jkanawalliii@earthlink.net>
2013-01-12 16:18:47 -0500

    added new blocks

--- init.lua
+++ init.lua
@@ -5,6 +5,17 @@ dofile(minetest.get_modpath("spleef_arena").."/recipes.lua");
 --Node Descriptions
 --
 
+minetest.register_node("spleef_arena:spleef_block_component", {
+	description = "Spleef Block Component",
+	drawtype = "glasslike",
+	visualscale = 1.0,
+	paramtype = "light",
+	sunlight_propogates = false,
+	walkable = true,
+	tiles = {"spleef_arena_component.png"},
+	groups = {spleef=2}
+	})
+
 minetest.register_node("spleef_arena:spleef_block_dirt", {
 	description = "Dirt Spleef Block",
 	drawtype = "glasslike",
@@ -17,15 +28,64 @@ minetest.register_node("spleef_arena:spleef_block_dirt", {
 	groups = {spleef=3}
 	})
 
-minetest.register_node("spleef_arena:spleef_block_component", {
-	description = "Spleef Block Component",
+minetest.register_node("spleef_arena:spleef_block_glass", {
+	description = "Glass Spleef Block",
 	drawtype = "glasslike",
 	visualscale = 1.0,
 	paramtype = "light",
 	sunlight_propogates = false,
 	walkable = true,
-	tiles = {"spleef_arena_component.png"},
-	groups = {spleef=2}
+	tiles = {"spleef_arena_glass.png"},
+	light_source = 2,
+	groups = {spleef=3}
+	})
+
+minetest.register_node("spleef_arena:spleef_block_desert_sand", {
+	description = "Desert Sand Spleef Block",
+	drawtype = "glasslike",
+	visualscale = 1.0,
+	paramtype = "light",
+	sunlight_propogates = false,
+	walkable = true,
+	tiles = {"spleef_arena_desert_sand.png"},
+	light_source = 2,
+	groups = {spleef=3}
+	})
+
+	minetest.register_node("spleef_arena:spleef_block_gravel", {
+	description = "Gravel Spleef Block",
+	drawtype = "glasslike",
+	visualscale = 1.0,
+	paramtype = "light",
+	sunlight_propogates = false,
+	walkable = true,
+	tiles = {"spleef_arena_gravel.png"},
+	light_source = 2,
+	groups = {spleef=3}
+	})
+
+minetest.register_node("spleef_arena:spleef_block_sand", {
+	description = "Sand Spleef Block",
+	drawtype = "glasslike",
+	visualscale = 1.0,
+	paramtype = "light",
+	sunlight_propogates = false,
+	walkable = true,
+	tiles = {"spleef_arena_sand.png"},
+	light_source = 2,
+	groups = {spleef=3}
+	})
+
+minetest.register_node("spleef_arena:spleef_block_leaves", {
+	description = "Leaves Spleef Block",
+	drawtype = "glasslike",
+	visualscale = 1.0,
+	paramtype = "light",
+	sunlight_propogates = false,
+	walkable = true,
+	tiles = {"spleef_arena_leaves.png"},
+	light_source = 2,
+	groups = {spleef=3}
 	})
 
 --
@@ -51,8 +111,13 @@ minetest.register_tool("spleef_arena:spleef_stick", {
 --Aliases
 --
 minetest.register_alias("spleef_stick", "spleef_arena:spleef_stick")
-minetest.register_alias("spleef_dirt", "spleef_arena:spleef_block_dirt")
 minetest.register_alias("spleef_component", "spleef_arena:spleef_block_component")
+minetest.register_alias("spleef_dirt", "spleef_arena:spleef_block_dirt")
+minetest.register_alias("spleef_gravel", "spleef_arena:spleef_block_gravel")
+minetest.register_alias("spleef_sand", "spleef_arena:spleef_block_sand")
+minetest.register_alias("spleef_desert_sand", "spleef_arena:spleef_block_desert_sand")
+minetest.register_alias("spleef_glass", "spleef_arena:spleef_block_glass")
+minetest.register_alias("spleef_leaves", "spleef_arena:spleef_block_leaves")
 
 
 print("Spleef Arena loaded! by jojoa1997!")
--- recipes.lua
+++ recipes.lua
@@ -1,6 +1,15 @@
 --Spleef Arena Mod Recipes
 
 minetest.register_craft({
+	output = 'spleef_arena:spleef_block_component',
+	recipe = {
+			{'default:glass', '', 'default:glass'},
+			{'', 'default:glass', ''},
+			{'default:glass', '', 'default:glass'},
+	}
+})
+
+minetest.register_craft({
 	output = 'spleef_arena:spleef_block_dirt',
 	recipe = {
 			{'default:dirt', 'default:dirt', 'default:dirt'},
@@ -10,11 +19,47 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = 'spleef_arena:spleef_block_component',
+	output = 'spleef_arena:spleef_block_gravel',
 	recipe = {
-			{'default:glass', '', 'default:glass'},
-			{'', 'default:glass', ''},
-			{'default:glass', '', 'default:glass'},
+			{'default:gravel', 'default:gravel', 'default:gravel'},
+			{'default:gravel', 'spleef_arena:spleef_block_component', 'default:gravel'},
+			{'default:gravel', 'default:gravel', 'default:gravel'},
+	}
+})
+
+minetest.register_craft({
+	output = 'spleef_arena:spleef_block_sand',
+	recipe = {
+			{'default:sand', 'default:sand', 'default:sand'},
+			{'default:sand', 'spleef_arena:spleef_block_component', 'default:sand'},
+			{'default:sand', 'default:sand', 'default:sand'},
+	}
+})
+
+minetest.register_craft({
+	output = 'spleef_arena:spleef_block_desert_sand',
+	recipe = {
+			{'default:desert_sand', 'default:desert_sand', 'default:desert_sand'},
+			{'default:desert_sand', 'spleef_arena:spleef_block_component', 'default:desert_sand'},
+			{'default:desert_sand', 'default:desert_sand', 'default:desert_sand'},
+	}
+})
+
+minetest.register_craft({
+	output = 'spleef_arena:spleef_block_glass',
+	recipe = {
+			{'default:glass', 'default:glass', 'default:glass'},
+			{'default:glass', 'spleef_arena:spleef_block_component', 'default:glass'},
+			{'default:glass', 'default:glass', 'default:glass'},
+	}
+})
+
+minetest.register_craft({
+	output = 'spleef_arena:spleef_block_leaves',
+	recipe = {
+			{'default:leaves', 'default:leaves', 'default:leaves'},
+			{'default:leaves', 'spleef_arena:spleef_block_component', 'default:leaves'},
+			{'default:leaves', 'default:leaves', 'default:leaves'},
 	}
 })
 
